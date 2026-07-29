9ddd906c4b5321a6c7c9fc341109e0b02bd0e5fa
colafang <colafang233@gmail.com>
2026-07-29 23:24:06 +0800

    Add contract domain events

--- packages/core/src/events.ts
+++ packages/core/src/events.ts
@@ -1,5 +1,6 @@
 import type { HexCoord } from "./hex";
-import type { BattleId, UnitId } from "./ids";
+import type { BattleId, FactionId, UnitId } from "./ids";
+import type { CommandSealEffect } from "./state";
 
 export interface UnitMovedEvent {
   readonly type: "battle.unit_moved";
@@ -62,6 +63,98 @@ export interface BattleTurnAdvancedEvent {
   readonly round: number;
 }
 
+export interface MasterGuardedEvent {
+  readonly type: "contract.master_guarded";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly masterId: UnitId;
+  readonly guardianId: UnitId;
+  readonly attackerId: UnitId;
+}
+
+export interface ManaTransferredEvent {
+  readonly type: "contract.mana_transferred";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly factionId: FactionId;
+  readonly masterId: UnitId;
+  readonly servantId: UnitId;
+  readonly amount: number;
+}
+
+export interface ServantUpkeepPaidEvent {
+  readonly type: "contract.servant_upkeep_paid";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly factionId: FactionId;
+  readonly servantId: UnitId;
+  readonly amount: number;
+  readonly required: number;
+}
+
+export interface LowManaStateChangedEvent {
+  readonly type: "contract.low_mana_changed";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+  readonly lowMana: boolean;
+}
+
+export interface ContractStabilityChangedEvent {
+  readonly type: "contract.stability_changed";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly factionId: FactionId;
+  readonly stability: number;
+}
+
+export interface CommandSealUsedEvent {
+  readonly type: "contract.command_seal_used";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly factionId: FactionId;
+  readonly effect: CommandSealEffect;
+}
+
+export interface ServantRecalledEvent {
+  readonly type: "contract.servant_recalled";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+  readonly from: HexCoord;
+  readonly to: HexCoord;
+}
+
+export interface ExtraTurnGrantedEvent {
+  readonly type: "contract.extra_turn_granted";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+}
+
+export interface ManaRestoredEvent {
+  readonly type: "contract.mana_restored";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+  readonly amount: number;
+}
+
+export interface DeathWardActivatedEvent {
+  readonly type: "contract.death_ward_activated";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+}
+
+export interface DeathRejectedEvent {
+  readonly type: "contract.death_rejected";
+  readonly sequence: number;
+  readonly battleId: BattleId;
+  readonly servantId: UnitId;
+  readonly sourceId: UnitId;
+}
+
 export type DomainEvent =
   | UnitMovedEvent
   | AttackStartedEvent
@@ -69,4 +162,15 @@ export type DomainEvent =
   | ReactionSpentEvent
   | DamageDealtEvent
   | UnitDefeatedEvent
-  | BattleTurnAdvancedEvent;
+  | BattleTurnAdvancedEvent
+  | MasterGuardedEvent
+  | ManaTransferredEvent
+  | ServantUpkeepPaidEvent
+  | LowManaStateChangedEvent
+  | ContractStabilityChangedEvent
+  | CommandSealUsedEvent
+  | ServantRecalledEvent
+  | ExtraTurnGrantedEvent
+  | ManaRestoredEvent
+  | DeathWardActivatedEvent
+  | DeathRejectedEvent;
